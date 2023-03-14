d3a8c4407ec00da4f10b55bb5b221bc83c96ab27
heloisasantana <heloisasantana.silva@hotmail.com>
2023-03-14 18:30:02 -0300

    Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,16 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, headers={"user-agent": "Fake user-agent"})
+        response.raise_for_status()
+        return response.text
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
 
 
 # Requisito 2
