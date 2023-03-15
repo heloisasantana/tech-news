a2ee9827353d929b534f1eb9adfb5db2a8ed1b4b
heloisasantana <heloisasantana.silva@hotmail.com>
2023-03-15 11:56:10 -0300

    Requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -23,7 +23,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_link = selector.css(".next::attr(href)").get()
+    return next_link
 
 
 # Requisito 4
